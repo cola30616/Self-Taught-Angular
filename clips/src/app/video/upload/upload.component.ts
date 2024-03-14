d5d4dd8eb4858415b3c8d59eb6bff721beae1cce
子謙 林 <cola30616@gmail.com>
2024-03-14 21:39:59 +0800

    影片剪輯清單

--- clips/src/app/video/upload/upload.component.ts
+++ clips/src/app/video/upload/upload.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -9,11 +9,23 @@ export class UploadComponent {
   isDragOver = false;
   file: File | null = null;
   nextStep = false;
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
   storeFile($event: Event) {
     this.isDragOver = false;
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
     if (!this.file || this.file.type !== 'video/mp4') return;
-
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
+  }
+
+  uploadFile() {
+    console.log('File uploaded');
   }
 }
