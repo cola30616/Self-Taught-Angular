df67e6832358cf95c3407080dfc6c5a0070b0b5a
Clement Lin <cola30616@gmail.com>
2024-03-15 16:34:08 +0800

    03/15

--- clips/src/app/video/upload/upload.component.ts
+++ clips/src/app/video/upload/upload.component.ts
@@ -1,7 +1,11 @@
+import firebase from 'firebase/compat/app';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
+import { last, switchMap } from 'rxjs/operators';
+import { pipe } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -19,8 +23,16 @@ export class UploadComponent {
   alertColor = 'blue';
   alertMsg = '請稍等，您的影片正在上傳';
   inSubmission = false;
+  percentage = 0;
+  showPercentage = false;
+  user: firebase.User | null = null;
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth
+  ) {
+    auth.user.subscribe((user) => (this.user = user));
+  }
 
   uploadForm = new FormGroup({
     title: this.title,
@@ -38,9 +50,43 @@ export class UploadComponent {
     this.alertColor = 'blue';
     this.alertMsg = '請稍等，您的影片正在上傳';
     this.inSubmission = true;
+    this.showPercentage = true;
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
-    this.storage.upload(clipPath, this.file);
+    const task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+    task.percentageChanges().subscribe((progress) => {
+      this.percentage = (progress as number) / 100;
+    });
+
+    task
+      .snapshotChanges()
+      .pipe(
+        last(),
+        switchMap(() => clipRef.getDownloadURL())
+      )
+      .subscribe({
+        next: (url) => {
+          const clip = {
+            uid: this.user?.uid,
+            displayName: this.user?.displayName,
+            title: this.title.value,
+            fileName: `${clipFileName}.mp4`,
+            url,
+          };
+          console.log(clip);
+          this.alertColor = 'green';
+          this.alertMsg = '成功上傳影片，您的剪輯已發布';
+          this.showPercentage = false;
+        },
+        error: (error) => {
+          this.alertColor = 'red';
+          this.alertMsg = '上傳失敗，請稍後再嘗試';
+          this.inSubmission = true;
+          this.showPercentage = false;
+          console.log(error);
+        },
+      });
   }
 }
